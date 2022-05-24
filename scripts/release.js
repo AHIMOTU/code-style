d11573886d906ce26fd9cdf4f90be711aa71c8c7
zhangxiang <zhangxiang@vzan.com>
2022-05-24 12:26:13 +0800

    ci: add release script

--- scripts/release.js
+++ scripts/release.js
@@ -11,6 +11,7 @@ const currentVersion = require('../package.json').version
 const preId =
   args.preid ||
   (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0])
+const isDryRun = args.dry
 const versionIncrements = [
   'patch',
   'minor',
@@ -21,6 +22,9 @@ const inc = (i) => semver.inc(currentVersion, i, preId)
 const step = (msg) => console.log(chalk.cyan(msg))
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
+const dryRun = (bin, args, opts = {}) =>
+  console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
+const runIfNotDry = isDryRun ? dryRun : run
 
 async function main() {
   const { release } = await prompt({
@@ -42,17 +46,52 @@ async function main() {
   updatePackage(path.resolve(__dirname, '../package.json'), targetVersion)
 
   const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
-  if (stdout) {}
+  if (stdout) {
+    step('\nCommitting changes...')
+    await runIfNotDry('git', ['add', '-A'])
+    await runIfNotDry('git', ['commit', '-m', `release: v${targetVersion}`])
+  } else {
+    console.log('No changes to commit.')
+  }
 
   step('\nPublishing package...')
+  await publishPackage(targetVersion)
 
   step('\nPushing to Github...')
+  await runIfNotDry('git', ['tag', `v${targetVersion}`])
+  await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
+  await runIfNotDry('git', ['push'])
+
+  if (isDryRun) {
+    console.log(`\nDry run finished - run git diff to see package changes.`)
+  }
 }
 
 function updatePackage(pkgPath, version) {
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
   pkg.version = version
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+}
+
+async function publishPackage(version) {
+  let releaseTag = null
+  if (args.tag) {
+    releaseTag = args.tag
+  } else if (version.includes('alpha')) {
+    releaseTag = 'alpha'
+  } else if (version.includes('beta')) {
+    releaseTag = 'beta'
+  } else if (version.includes('rc')) {
+    releaseTag = 'rc'
+  }
+
+  await runIfNotDry('npm', [
+    'publish',
+    ...(releaseTag ? ['--tag', releaseTag] : []),
+    '--access',
+    'public'
+  ], { stdio: 'pipe' })
+  console.log(chalk.green(`Successfully published ${version}`))
 }
 
 main().catch((error) => {
